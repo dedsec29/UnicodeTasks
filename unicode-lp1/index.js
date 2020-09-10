66307c95ff8257d0bf6a316838fe10a9d51fb969
Parth Pawar <parth290601@rediffmail.com>
2020-09-10 17:05:27 +0530

    Restructuring

--- unicode-lp1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-//initialising routes
-app.use('/api/repositories', require('./routes/repositories_api'));
-app.use('/api/users', require('./routes/users_api'));
-
-const port = process.env.PORT || 3000;	
-app.listen(port, ()=> console.log(`Listening on port ${port}`));
